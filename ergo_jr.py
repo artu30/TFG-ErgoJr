c643331cbb353cc39e6020bfbdc040205c19a20e
Artu <gonzalez.arturo1991@gmail.com>
2021-05-23 16:53:30 +0200

    Update ergo_jr.py

--- ergo_jr.py
+++ ergo_jr.py
@@ -4,7 +4,5 @@ import time
 poppy = PoppyErgoJr(camera='dummy')
 
 for _ in range(3):
-    poppy.m3.goal_position = 30
-    time.sleep(0.5)
-    poppy.m3.goal_position = -30
-    time.sleep(0.5)
+    poppy.m3.goto_position(30, 0.5, wait=True)
+    poppy.m3.goto_position(-30, 0.5, wait=True)
