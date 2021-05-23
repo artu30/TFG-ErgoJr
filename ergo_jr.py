8123990266ad409df69d269ea58d35822b2bfad6
Artu <gonzalez.arturo1991@gmail.com>
2021-05-23 17:31:40 +0200

    Update ergo_jr.py

--- ergo_jr.py
+++ ergo_jr.py
@@ -1,16 +1,5 @@
-import logging, sys
 from pypot.creatures import PoppyErgoJr
-import time
-
-logging.basicConfig(stream=sys.stderr, level="DEBUG")
 
 poppy = PoppyErgoJr(camera='dummy')
 
-poppy.m3.compliant = False
-poppy.m3.goal_position = 0
-
-for _ in range(3):
-    poppy.m3.goal_position = 30
-    time.sleep(0.5)
-    poppy.m3.goal_position = -30
-    time.sleep(0.5)
+poppy.rest_posture.start()
