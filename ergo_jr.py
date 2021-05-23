5f7b77a2b11e662465f65ecb14abc66aea2b5654
Artu <gonzalez.arturo1991@gmail.com>
2021-05-23 17:00:34 +0200

    Update ergo_jr.py

--- ergo_jr.py
+++ ergo_jr.py
@@ -4,11 +4,4 @@ import time
 poppy = PoppyErgoJr(camera='dummy')
 
 for m in poppy.motors:
-    m.compliant = True
-
-poppy.goto_position({'m1': 0.,
-                'm2': -60.,
-                'm3': 55.,
-                'm4': 0.,
-                'm5': -55.,
-                'm6': 60.}, 2., wait=True)
+    print(m.present_position)
