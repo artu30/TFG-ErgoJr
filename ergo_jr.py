95c0d386027133eadc2bb81bda8ed44cd612e81a
Artu <gonzalez.arturo1991@gmail.com>
2021-05-24 13:19:47 +0200

    Update ergo_jr.py

--- ergo_jr.py
+++ ergo_jr.py
@@ -4,5 +4,5 @@ poppy = PoppyErgoJr(camera='dummy')
 
 poppy.m1.goal_position = -20
 poppy.m2.goal_position = -30
-poppy.m2.goal_position = -30
-poppy.m2.goal_position = -30
+poppy.m3.goal_position = -30
+poppy.m4.goal_position = -30
