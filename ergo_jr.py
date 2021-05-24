1ff01b816085da28caeb27aab8da0906b00969b2
Artu <gonzalez.arturo1991@gmail.com>
2021-05-24 13:24:43 +0200

    Update ergo_jr.py

--- ergo_jr.py
+++ ergo_jr.py
@@ -1,9 +1,10 @@
-from pypot.creatures import PoppyErgoJr
+try:
+    from pypot.creatures import PoppyErgoJr
+    poppy = PoppyErgoJr(camera='dummy')
+except Exception,e:
+    print "could not create poppy object"
+    print e
 
-poppy = PoppyErgoJr(camera='dummy')
-
-poppy.m3.compliant = False
-poppy.m1.goal_position = -20
-poppy.m2.goal_position = -30
-poppy.m3.goal_position = -30
-poppy.m4.goal_position = -30
+for m in poppy.motors:
+    m.compliant = False
+    m.goal_position = 0.0
