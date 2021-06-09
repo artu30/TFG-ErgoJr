9ea490ea58bb67d9b549738dde2e86c673f8a44e
Artu <gonzalez.arturo1991@gmail.com>
2021-06-09 19:29:03 +0200

    Update ergo_jr.py

--- ergo_jr.py
+++ ergo_jr.py
@@ -8,6 +8,6 @@ poppy.m3.compliant = False
 
 print(poppy.m3.present_position)
 
-poppy.m1.goal_position = 0
-poppy.m2.goal_position = 0
-poppy.m3.goal_position = 0
+poppy.m1.goal_position = -30
+poppy.m2.goal_position = -20
+poppy.m3.goal_position = -10
