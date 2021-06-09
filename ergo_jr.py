bc3e1d616a14ddd26d6263e514b953aac165f873
Artu <gonzalez.arturo1991@gmail.com>
2021-06-09 17:45:39 +0200

    Update ergo_jr.py

--- ergo_jr.py
+++ ergo_jr.py
@@ -2,9 +2,6 @@ from pypot.creatures import PoppyErgoJr
 
 poppy = PoppyErgoJr(camera='dummy')
 
-poppy.m1.goal_position = 0
-poppy.m2.goal_position = -15
-poppy.m3.goal_position = 40
-poppy.m4.goal_position = 0
-poppy.m5.goal_position = -35
-poppy.m6.goal_position = -60
+poppy.m3.compliant = False
+
+poppy.m3.goal_position = 30
