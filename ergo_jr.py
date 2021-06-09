8a46e5fe9d1be9c615e35b24329923176150d5a5
Artu <gonzalez.arturo1991@gmail.com>
2021-06-09 20:18:47 +0200

    Update ergo_jr.py

--- ergo_jr.py
+++ ergo_jr.py
@@ -14,21 +14,30 @@ def getchar():
       termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
    return ch
 
-def executeExample():
-    poppy = PoppyErgoJr(camera='dummy')
-
+def InitializeErgoJr(poppy):
     poppy.m1.compliant = False
     poppy.m2.compliant = False
     poppy.m3.compliant = False
 
-    poppy.m1.moving_speed = 250
-    poppy.m2.moving_speed = 250
-    poppy.m3.moving_speed = 250
+    poppy.m1.moving_speed = 200
+    poppy.m2.moving_speed = 200
+    poppy.m3.moving_speed = 200
 
+def executeExample(poppy):
     poppy.m1.goal_position = 10
     poppy.m2.goal_position = 10
     poppy.m3.goal_position = 10
+    poppy.m4.goal_position = 10
+    poppy.m5.goal_position = 10
+    poppy.m6.goal_position = 10
 
+def BackToRestPosture(poppy):
+    poppy.m1.goal_position = 0
+    poppy.m2.goal_position = 0
+    poppy.m3.goal_position = 0
+    poppy.m4.goal_position = 0
+    poppy.m5.goal_position = 0
+    poppy.m6.goal_position = 0
 
 print("Write a character")
 
@@ -38,8 +47,17 @@ while 1:
         print('bye!')
         break
     else:
-        executeExample()
-        time.sleep(6)
+        print('Writting . . .')
+
+        poppy = PoppyErgoJr(camera='dummy')
+
+        InitializeErgoJr(poppy)
+
+        executeExample(poppy)
+        time.sleep(4)
+        
+        BackToRestPosture(poppy)
+        time.sleep(4)
         break
 
 
