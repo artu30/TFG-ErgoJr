c4c37f1ab2b189cb34a322e59ce64ef49b9e0f29
Artu <gonzalez.arturo1991@gmail.com>
2021-06-09 19:38:27 +0200

    Update ergo_jr.py

--- ergo_jr.py
+++ ergo_jr.py
@@ -10,8 +10,6 @@ poppy.m1.moving_speed = 250
 poppy.m2.moving_speed = 250
 poppy.m3.moving_speed = 250
 
-print(poppy.m3.present_position)
-
 poppy.m1.goal_position = 0
 poppy.m2.goal_position = 0
 poppy.m3.goal_position = 0
