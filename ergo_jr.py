6253f68142cdfaace4fa9fc171768f1decc21e4d
Artu <gonzalez.arturo1991@gmail.com>
2021-06-09 20:12:27 +0200

    Update ergo_jr.py

--- ergo_jr.py
+++ ergo_jr.py
@@ -14,6 +14,22 @@ def getchar():
       termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
    return ch
 
+def executeExample():
+    poppy = PoppyErgoJr(camera='dummy')
+
+    poppy.m1.compliant = False
+    poppy.m2.compliant = False
+    poppy.m3.compliant = False
+
+    poppy.m1.moving_speed = 250
+    poppy.m2.moving_speed = 250
+    poppy.m3.moving_speed = 250
+
+    poppy.m1.goal_position = 0
+    poppy.m2.goal_position = 0
+    poppy.m3.goal_position = 0
+
+
 print("Write a character")
 
 while 1:
@@ -21,21 +37,11 @@ while 1:
     if ch.strip() == '':
         print('bye!')
         break
+    else:
+        print 'You pressed', ch
+        executeExample()
+        time.sleep(6)
+        break
 
-print(ch)
 
-poppy = PoppyErgoJr(camera='dummy')
 
-poppy.m1.compliant = False
-poppy.m2.compliant = False
-poppy.m3.compliant = False
-
-poppy.m1.moving_speed = 250
-poppy.m2.moving_speed = 250
-poppy.m3.moving_speed = 250
-
-poppy.m1.goal_position = 0
-poppy.m2.goal_position = 0
-poppy.m3.goal_position = 0
-
-time.sleep(6)
