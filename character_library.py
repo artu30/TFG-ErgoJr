b2295d94e5d2953a4bd823e8a0432f4da9a8f34d
Artu <gonzalez.arturo1991@gmail.com>
2021-06-20 17:18:55 +0200

    Update character_library.py

--- character_library.py
+++ character_library.py
@@ -7,11 +7,15 @@ class CharacterLibrary:
 
    def WriteA(self):
       self.poppy.m1.moving_speed = 20
+      self.poppy.m2.moving_speed = 40
       self.poppy.m3.moving_speed = 40
       self.poppy.m5.moving_speed = 40
       
-      
-      time.sleep(10)
+      poppy.m1.goal_position = 0
+      poppy.m2.goal_position = 30
+      poppy.m5.goal_position = -60
+      poppy.m3.goal_position = 60
+      time.sleep(4)
 
    def WriteB(self):
       print ("B")
