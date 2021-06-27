9fc40637051b214018734ebf413f9084063f3e1a
Artu <gonzalez.arturo1991@gmail.com>
2021-06-27 19:03:25 +0200

    Update ergo_jr.py

--- ergo_jr.py
+++ ergo_jr.py
@@ -4,6 +4,7 @@ import os
 import glob
 import time
 import RPi.GPIO as GPIO
+import socket
 from bluetooth import *
 from character_library import *
 from pypot.creatures import PoppyErgoJr
@@ -51,44 +52,45 @@ advertise_service( server_sock, "AquaPiServer",
                    service_id = uuid,
                    service_classes = [ uuid, SERIAL_PORT_CLASS ],
                    profiles = [ SERIAL_PORT_PROFILE ]
+#                   protocols = [ OBEX_UUID ] 
                     )
 
 while True:
     print ("Waiting for connection on RFCOMM channel %d", port)
 
-	client_sock = server_sock.accept()
+    client_sock = server_sock.accept()
     
     poppy = PoppyErgoJr(camera='dummy')
 
     CharacterManager = CharacterLibrary(poppy)
 
     operaciones = { 
-    'a': CharacterManager.WriteA, 
-    'b': CharacterManager.WriteB, 
-    'c': CharacterManager.WriteC, 
-    'd': CharacterManager.WriteD,
-    'e': CharacterManager.WriteE, 
-    'f': CharacterManager.WriteF, 
-    'g': CharacterManager.WriteG, 
-    'h': CharacterManager.WriteH,
-    'i': CharacterManager.WriteI, 
-    'j': CharacterManager.WriteJ, 
-    'k': CharacterManager.WriteQ, 
-    'l': CharacterManager.WriteL,
-    'm': CharacterManager.WriteM, 
-    'n': CharacterManager.WriteN, 
-    'o': CharacterManager.WriteO, 
-    'p': CharacterManager.WriteP,
-    'q': CharacterManager.WriteQ, 
-    'r': CharacterManager.WriteR, 
-    's': CharacterManager.WriteS, 
-    't': CharacterManager.WriteT,
-    'u': CharacterManager.WriteU, 
-    'v': CharacterManager.WriteV, 
-    'w': CharacterManager.WriteW, 
-    'x': CharacterManager.WriteX,
-    'y': CharacterManager.WriteY, 
-    'z': CharacterManager.WriteZ
+        'a': CharacterManager.WriteA, 
+        'b': CharacterManager.WriteB, 
+        'c': CharacterManager.WriteC, 
+        'd': CharacterManager.WriteD,
+        'e': CharacterManager.WriteE, 
+        'f': CharacterManager.WriteF, 
+        'g': CharacterManager.WriteG, 
+        'h': CharacterManager.WriteH,
+        'i': CharacterManager.WriteI, 
+        'j': CharacterManager.WriteJ, 
+        'k': CharacterManager.WriteQ, 
+        'l': CharacterManager.WriteL,
+        'm': CharacterManager.WriteM, 
+        'n': CharacterManager.WriteN, 
+        'o': CharacterManager.WriteO, 
+        'p': CharacterManager.WriteP,
+        'q': CharacterManager.WriteQ, 
+        'r': CharacterManager.WriteR, 
+        's': CharacterManager.WriteS, 
+        't': CharacterManager.WriteT,
+        'u': CharacterManager.WriteU, 
+        'v': CharacterManager.WriteV, 
+        'w': CharacterManager.WriteW, 
+        'x': CharacterManager.WriteX,
+        'y': CharacterManager.WriteY, 
+        'z': CharacterManager.WriteZ
     }
 
     InitializeErgoJr(poppy)
