4ed59d7e6be0ba98bb7fa31b7cc416805e8b88af
Artu <gonzalez.arturo1991@gmail.com>
2021-07-04 18:39:30 +0200

    Update character_library.py

--- character_library.py
+++ character_library.py
@@ -72,7 +72,10 @@ class CharacterLibrary:
       self.poppy.m3.goal_position = 60
       time.sleep(1)
 
-      self.poppy.m1.goal_position = -5
+      self.poppy.m1.goal_position = -10
+      self.poppy.m2.goal_position = 15
+      self.poppy.m5.goal_position = -75
+      self.poppy.m3.goal_position = 75
       time.sleep(1)
 
       self.poppy.m2.goal_position = 15
