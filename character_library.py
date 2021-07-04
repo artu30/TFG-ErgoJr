3a0557ed044c32160dd5ae188043f959f056048c
Artu <gonzalez.arturo1991@gmail.com>
2021-07-04 17:57:40 +0200

    Update character_library.py

--- character_library.py
+++ character_library.py
@@ -66,37 +66,29 @@ class CharacterLibrary:
       self.poppy.m3.moving_speed = 40
       self.poppy.m5.moving_speed = 40
 
-      self.poppy.m2.goal_position = 30
+      self.poppy.m2.goal_position = 40
       self.poppy.m5.goal_position = -60
       self.poppy.m3.goal_position = 60
       time.sleep(1)
 
-      self.poppy.m1.goal_position = -5
+      self.poppy.m1.goal_position = -20
       time.sleep(0.5)
-      self.poppy.m5.goal_position = -67.5
-      time.sleep(0.5)
-      self.poppy.m3.goal_position = 67.5
+      self.poppy.m2.goal_position = 30
       time.sleep(1)
 
       self.poppy.m1.goal_position = 0
       time.sleep(0.5)
-      self.poppy.m5.goal_position = -75
-      time.sleep(0.5)
-      self.poppy.m3.goal_position = 75
+      self.poppy.m2.goal_position = 20
       time.sleep(1)
 
-      self.poppy.m1.goal_position = -5
+      self.poppy.m1.goal_position = -20
       time.sleep(0.5)
-      self.poppy.m5.goal_position = -82.5
-      time.sleep(0.5)
-      self.poppy.m3.goal_position = 82.5
+      self.poppy.m2.goal_position = 10
       time.sleep(0.5)
 
-      self.poppy.m1.goal_position = -5
+      self.poppy.m1.goal_position = 0
       time.sleep(0.5)
-      self.poppy.m5.goal_position = -90
-      time.sleep(0.5)
-      self.poppy.m3.goal_position = 90
+      self.poppy.m2.goal_position = 0
       time.sleep(1)
 
    def WriteC(self):
