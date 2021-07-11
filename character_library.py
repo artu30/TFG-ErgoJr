400a9eda66d74630f385b08409e88758984c83d9
Artu <gonzalez.arturo1991@gmail.com>
2021-07-11 19:10:57 +0200

    Update character_library.py

--- character_library.py
+++ character_library.py
@@ -24,27 +24,27 @@ class CharacterLibrary:
       self.poppy.m3.goal_position = 90
       time.sleep(2)
 
+      self.poppy.m2.goal_position = 0
       self.poppy.m6.goal_position = -10
-      time.sleep(0.5)
+      time.sleep(1)
       self.poppy.m3.goal_position = 0
       time.sleep(0.5)
       self.poppy.m5.goal_position = 0
       time.sleep(0.5)
       self.poppy.m1.goal_position = 0
       time.sleep(0.5)
-      self.poppy.m2.goal_position = 0
-      time.sleep(0.5)
 
       self.poppy.m6.goal_position = 0
+      time.sleep(2)
+
+      self.poppy.m5.goal_position = -60
       time.sleep(0.5)
+      self.poppy.m3.goal_position = 60
+      time.sleep(2)
 
       self.poppy.m2.goal_position = 15
-      time.sleep(0.5)
-      self.poppy.m5.goal_position = -75
-      time.sleep(0.5)
-      self.poppy.m3.goal_position = 75
-      time.sleep(2)
-      
+      time.sleep(1)
+
       self.poppy.m1.goal_position = 6
       time.sleep(0.5)
       self.poppy.m1.goal_position = -6
